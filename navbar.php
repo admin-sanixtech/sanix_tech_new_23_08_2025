0ba154533c989f32f86010a9d421868546a6ce9b
SanixTech <noreply@sanixtech.in>
2025-07-23 21:34:54 +0530

    sanix tech update on 23-07-2025

--- navbar.php
+++ navbar.php
@@ -5,41 +5,41 @@
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services</a>
                 <ul class="dropdown-menu">
                     <li><a href="https://www.sanixtech.in/web_development.php">Web Development</a></li>
-                    <li><a href="#">Industry Standard Projects</a></li>
-                    <li><a href="#">Acedemic Projects</a></li>
-                    <li><a href="#">Data Migration</a></li>
+                    <li><a href="https://www.sanixtech.in/industry_st_projects.php">Industry Standard Projects</a></li>
+                    <li><a href="https://www.sanixtech.in/academic_projects.php">Acedemic Projects</a></li>
+                    <li><a href="https://www.sanixtech.in/data_migration.php">Data Migration</a></li>
                 </ul>
             </li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Courses</a>
                 <ul class="dropdown-menu">
                     <li><a href="https://www.sanixtech.in/python_course.php">Python Programming</a></li>
-                    <li><a href="#">Data Science</a></li>
+                    <li><a href="https://www.sanixtech.in/datascience_course.php">Data Science</a></li>
                     <li><a href="https://www.sanixtech.in/web_development.php">Web Development</a></li>
-                    <li><a href="#">Machine Learning</a></li>
+                    <li><a href="https://www.sanixtech.in/machine_learning.php">Machine Learning</a></li>
                 </ul>
             </li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Projects</a>
                 <ul class="dropdown-menu">
                     <li><a href="https://www.sanixtech.in/python_course.php">Python Programming</a></li>
-                    <li><a href="#">Data Science</a></li>
+                    <li><a href="https://www.sanixtech.in/datascience_projects.php">Data Science</a></li>
                     <li><a href="https://www.sanixtech.in/web_development.php">Web Development</a></li>
-                    <li><a href="#">Machine Learning</a></li>
+                    <li><a href="https://www.sanixtech.in/machine_learning_projects.php">Machine Learning</a></li>
                 </ul>
             </li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Interview Preparation</a>
                 <ul class="dropdown-menu">
                     <li><a href="https://www.sanixtech.in/mock_interviews.php">Mock Interview</a></li>
-                    <li><a href="https://www.sanixtech.in/mock_interviews.php">Quiz</a></li>
                     <li><a href="https://www.sanixtech.in/data_engineer_interview_prep.php">Data Engineering</a></li>
                     <li><a href="https://www.sanixtech.in/datascience_interview_prep.php">Data Science</a></li>
                     <li><a href="https://www.sanixtech.in/web_development.php">Web Development</a></li>
-                    <li><a href="#">Machine Learning</a></li>
+                    <li><a href="https://www.sanixtech.in/machine_learning_interview_prep.php">Machine Learning</a></li>
                 </ul>
             </li>
             <li><a href="careers.php">Careers</a></li>
+
             <li><a href="about_us.php">About Us</a></li>
             <li><a href="contact.php">Contact Us</a></li>
         </ul>
