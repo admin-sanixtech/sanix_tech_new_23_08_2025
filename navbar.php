90afbfef44efd842fa2e11efa8b6d396638c671f
SanixTech <noreply@sanixtech.in>
2025-07-26 23:00:23 +0530

    update on 26-07-2025 11pm

--- navbar.php
+++ navbar.php
@@ -4,12 +4,38 @@
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services</a>
                 <ul class="dropdown-menu">
+
+                    <!-- Core Services -->
                     <li><a href="https://www.sanixtech.in/web_development.php">Web Development</a></li>
-                    <li><a href="https://www.sanixtech.in/industry_st_projects.php">Industry Standard Projects</a></li>
-                    <li><a href="https://www.sanixtech.in/academic_projects.php">Acedemic Projects</a></li>
-                    <li><a href="https://www.sanixtech.in/data_migration.php">Data Migration</a></li>
+                    <li><a href="https://www.sanixtech.in/industry_st_projects.php">Industry-grade Project Solutions</a></li>
+                    <li><a href="https://www.sanixtech.in/academic_projects.php">Academic Project Support</a></li>
+                    <li><a href="https://www.sanixtech.in/data_migration.php">Data Migration Services</a></li>
+                    <li><a href="https://www.sanixtech.in/training_certification.php">Training & Certification</a></li>
+                    <li><a href="https://www.sanixtech.in/mock_interviews.php">Mock Interviews & Career Help</a></li>
+                    <li><a href="https://www.sanixtech.in/custom_solutions.php">Custom Software Solutions</a></li>
+                    <li><a href="https://www.sanixtech.in/internship_placement.php">Internship & Placement</a></li>
+
+                    <!-- Add-ons -->
+                    <li><a href="https://www.sanixtech.in/hosting_domain.php">Hosting & Domain Services</a></li>
+                    <li><a href="https://www.sanixtech.in/ui_ux_design.php">UI/UX Design</a></li>
+                    <li><a href="https://www.sanixtech.in/seo_marketing.php">SEO & Digital Marketing</a></li>
+
+                    <!-- Technology & Enterprise Services -->
+                    <li><a href="https://www.sanixtech.in/app_dev_maintenance.php">App Development & Maintenance</a></li>
+                    <li><a href="https://www.sanixtech.in/artificial_intelligence.php">Artificial Intelligence Solutions</a></li>
+                    <li><a href="https://www.sanixtech.in/cybersecurity.php">Cybersecurity Services</a></li>
+                    <li><a href="https://www.sanixtech.in/data_engineering.php">Data Engineering</a></li>
+                    <li><a href="https://www.sanixtech.in/enterprise_integration.php">Enterprise Apps & Integrations</a></li>
+                    <li><a href="https://www.sanixtech.in/generative_ai.php">Generative AI</a></li>
+                    <li><a href="https://www.sanixtech.in/infrastructure.php">IT Infrastructure Services</a></li>
+                    <li><a href="https://www.sanixtech.in/it_staffing.php">IT Staffing & Consulting</a></li>
+                    <li><a href="https://www.sanixtech.in/professional_services.php">Professional Consulting Services</a></li>
+                    <li><a href="https://www.sanixtech.in/qa_engineering.php">QA & Software Testing</a></li>
+                    <li><a href="https://www.sanixtech.in/product_engineering.php">Software Product Engineering</a></li>
+
                 </ul>
             </li>
+
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Courses</a>
                 <ul class="dropdown-menu">
